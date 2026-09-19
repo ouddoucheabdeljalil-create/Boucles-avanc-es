7990423c200bc203ce2a87a9e7cd39a2285a63b5
unknown <ouddoucheabdeljalil@gmail.com>
2026-09-19 09:59:03 +0200

    correction

--- challange2.js
+++ challange2.js
@@ -3,14 +3,6 @@ let num = Number(prompt("enter le numero :"));
 
 let x = "" ;
 
-for (let i = 1 ; i <= num ; i++){
-  x =  " ";
- for( let j = 1 ; j<= num -i ;j++ ){ 
-  x = x + " ";}
-  for(let j = 1 ; j <= 2 * i -1 ; j++){
-    x = x + "*"
-  }
-console.log(x );
-}
+
 
 
